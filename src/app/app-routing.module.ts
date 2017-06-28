98b0d2a835228eb7bbfde6941449ff5d8eb80851
matyi <matyas.csorba@melkweg.hu>
2017-06-28 17:33:12 +0200

    auth module started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
 import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {SigninComponent} from './auth/signin/signin.component';
 /**
  * Created by csmat on 2017. 06. 21..
  */
@@ -19,6 +21,8 @@ const appRoutes: Routes = [
         {path: ':id/edit', component: RecipeEditComponent}
     ]},
     {path: 'shopping-list', component: ShoppingListComponent},
+    {path: 'signup', component: SignupComponent},
+    {path: 'signin', component: SigninComponent},
 ];
 
 @NgModule({
