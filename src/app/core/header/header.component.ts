0263b1525b0852e173a25621c2a2c3383cc4d7ed
matyi <matyas.csorba@melkweg.hu>
2017-06-29 14:57:29 +0200

    restructuring for in time development

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -3,8 +3,8 @@
  */
 import {Component} from '@angular/core';
 import {Response} from '@angular/http';
-import {DataStorageService} from '../shared/data-storage.service';
-import {AuthService} from '../auth/auth.service';
+import {DataStorageService} from '../../shared/data-storage.service';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
     selector: 'app-header',
